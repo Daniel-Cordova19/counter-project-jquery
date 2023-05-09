80a758d688e8b4a7dbeaa4b75a68b90862655619
Daniel Cordova <javierdanielcordova@gmail.com>
2023-05-09 12:10:10 +0200

    ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 let counter = 0;
 
-$(document).ready(function () {
-    console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
+$(document).ready(function() {
+    var numero = 0;
+    $(".prevBtn").click(function() {
+      numero--;
+      $("#counter").text(numero);
+    });
+  });
 
+  $(document).ready(function() {
+    var numero = 0;
+    $(".nextBtn").click(function() {
+      numero++;
+      $("#counter").text(numero);
+    });
+  });
+
+$(document).ready(function () {
+    $(document).ready(() =>{
+        $(".prevBtn").click(() =>{
+        $('#counter').css("color", "red");
+        });
+    });
+});
+
+$(document).ready(function () {
+    $(document).ready(() =>{
+        $(".nextBtn").click(() =>{
+        $('#counter').css("color", "green");
+        });
+    });
 });
